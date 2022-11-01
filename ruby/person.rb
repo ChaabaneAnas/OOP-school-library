a8edbbab8c75da0262430a79d30d7b77b1b69fc5
Anas <anas.chaabane98@gmail.com>
2022-11-01 10:31:26 +0100

    final touches

--- ruby/person.rb
+++ ruby/person.rb
@@ -2,16 +2,15 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  # rubocop:disable Lint/UnusedMethodArgument
   def initialize(age, name = 'Unknownn', parent_permission: true)
-    # rubocop:enable Lint/UnusedMethodArgument
     @id = rand(1..100)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
-    true if age >= 18
+    @parent_permission || @age >= 18
   end
 
   private
