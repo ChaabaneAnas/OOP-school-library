e02c4785c00f2b30d36c2edce89c8580a3118b63
Anas <anas.chaabane98@gmail.com>
2022-11-02 16:41:34 +0100

    rubocop_color

--- ruby/Decorators/decorator.rb
+++ ruby/Decorators/decorator.rb
@@ -1,31 +1,28 @@
-
-
 class Nameable
   def correct_name
     raise NotImplementedError
-  end 
-end 
+  end
+end
 
 class BaseDecorator < Nameable
-  def initialize(nameableObj)
-    @NameableObj  = nameableObj
-  end 
+  def initialize(nameableobj)
+    super()
+    @nameable_obj = nameableobj
+  end
 
   def correct_name
-    @NameableObj.correct_name
-  end 
-end 
+    @nameable_obj.correct_name
+  end
+end
 
 class CapitalizeDecorator < BaseDecorator
   def correct_name
-    @NameableObj.correct_name.upcase
-  end 
-end 
+    @nameable_obj.correct_name.upcase
+  end
+end
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    @NameableObj.correct_name.slice(0..10)
-  end 
-end 
-
-
+    @nameable_obj.correct_name.slice(0..10)
+  end
+end
