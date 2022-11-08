15966bc1a86be15c755485ce461c4b242e2a0fc2
Anas <anas.chaabane98@gmail.com>
2022-11-08 13:37:29 +0100

    rubocop

--- main.rb
+++ main.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-require "./app"
+require './app'
 
 def main
-  app = App.new()
-  while 1 > 0
-  app.menu
-  user_input = gets.chomp.to_i
-  app.excute(user_input)
+  app = App.new
+  while 1.positive?
+    app.menu
+    user_input = gets.chomp.to_i
+    app.excute(user_input)
   end
-end 
-main()
+end
+main
